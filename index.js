a7d0112ded9dc7ad2115d0248b6d968cf6d7c064
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-30 06:57:51 +0600

    show on the UI in limited

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.connect(err => {
   const collection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.COLLECTION_NAME}`);
 
   app.get('/products', (req, res) => {
-    collection.find({})
+    collection.find({}).limit(4)
     .toArray((err, documents) => {
       res.send(documents);
     })
