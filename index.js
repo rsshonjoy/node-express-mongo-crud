df91422884ded0a70fa98e0c1ea0ce06c85973f2
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-30 06:33:59 +0600

    show data in console

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghjps.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
 client.connect(err => {
   const collection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.COLLECTION_NAME}`);
 
-  const product = {name: "modhu", price: 25, quantity: 20}
-  collection.insertOne(product)
-  .then(res => {
-    console.log('one product added');
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    console.log(product);
   })
   console.log('database connected');
 });
